f0df828a219070c9869bee65a782b2d5a9c97fa7
LdDl <sexykdi@gmail.com>
2024-12-13 18:51:35 +0300

    geojson for single mvmt

--- movement/geojson.go
+++ movement/geojson.go
@@ -1,6 +1,8 @@
 package movement
 
 import (
+	"strings"
+
 	"github.com/paulmach/orb/geojson"
 )
 
@@ -8,7 +10,32 @@ import (
 func (mvmt *Movement) GeoFeature() *geojson.Feature {
 	f := geojson.NewFeature(nil)
 	f.ID = mvmt.ID
-	panic("@tbd")
+	f.Properties["id"] = mvmt.ID
+	f.Properties["node_id"] = mvmt.MacroNode()
+	f.Properties["osm_node_id"] = mvmt.OSMNode()
+	f.Properties["name"] = mvmt.Name()
+	f.Properties["in_link_id"] = mvmt.IncomeMacroLink()
+	f.Properties["in_lane_start"] = mvmt.IncomeLaneStart()
+	f.Properties["in_lane_end"] = mvmt.IncomeLaneEnd()
+	f.Properties["out_link_id"] = mvmt.OutcomeMacroLink()
+	f.Properties["out_lane_start"] = mvmt.OutcomeLaneStart()
+	f.Properties["out_lane_end"] = mvmt.OutcomeLaneEnd()
+	f.Properties["lanes_num"] = mvmt.LanesNum()
+	f.Properties["from_osm_node_id"] = mvmt.OSMNodeFrom()
+	f.Properties["to_osm_node_id"] = mvmt.OSMNodeTo()
+	f.Properties["type"] = mvmt.Type()
+	f.Properties["penalty"] = -1  // @todo: future works
+	f.Properties["capacity"] = -1 // @todo: future works
+	f.Properties["control_type"] = mvmt.ControlType().String()
+	f.Properties["movement_composite_type"] = mvmt.MvmtTextID().String()
+	f.Properties["volume"] = -1     // @todo: future works
+	f.Properties["free_speed"] = -1 // @todo: future works
+	allowedAgentTypes := mvmt.AllowedAgentTypes()
+	allowedAgentTypesStrs := make([]string, len(mvmt.AllowedAgentTypes()))
+	for i, agentType := range allowedAgentTypes {
+		allowedAgentTypesStrs[i] = agentType.String()
+	}
+	f.Properties["allowed_agent_types"] = strings.Join(allowedAgentTypesStrs, ",")
 	return f
 }
 
