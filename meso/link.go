95aeea7ec52513729673166adf02286221903786
LdDl <sexykdi@gmail.com>
2024-12-15 17:00:10 +0300

    setters for meso link

--- meso/link.go
+++ meso/link.go
@@ -109,6 +109,16 @@ func (link *Link) TargetNode() gmns.NodeID {
 	return link.targetNodeID
 }
 
+// MacroNode returns identifier of parent macroscopic node. Outputs "-1" if it was not set.
+func (link *Link) MacroNode() gmns.NodeID {
+	return link.macroNodeID
+}
+
+// MacroLink returns identifier of parent macroscopic link. Outputs "-1" if it was not set.
+func (link *Link) MacroLink() gmns.LinkID {
+	return link.macroLinkID
+}
+
 // SegmentIdx returns segment index in parent macroscopic link.
 func (link *Link) SegmentIdx() int {
 	return link.segmentIdx
@@ -172,4 +182,159 @@ func (link *Link) Capacity() int {
 // AllowedAgentTypes returns set of allowed agent types. Warning: returning object is a slice.
 func (link *Link) AllowedAgentTypes() []types.AgentType {
 	return link.allowedAgentTypes
+}
+
+// WithLineGeom sets geometry [WGS84] for the link. Warning: it does not copy the given slice.
+func WithLineGeom(geom orb.LineString) func(*Link) {
+	return func(link *Link) {
+		link.geom = geom
+	}
+}
+
+// WithLineGeomEuclidean sets geometry [Euclidean] for the link. Warning: it does not copy the given slice.
+func WithLineGeomEuclidean(geomEuclidean orb.LineString) func(*Link) {
+	return func(link *Link) {
+		link.geomEuclidean = geomEuclidean
+	}
+}
+
+// WithLanesNum sets number of lanes for the link
+func WithLanesNum(lanesNum int) func(*Link) {
+	return func(link *Link) {
+		link.lanesNum = lanesNum
+	}
+}
+
+// WithLanesChange sets the lanes change array for the Link.
+func WithLanesChange(lanesChange [2]int) func(*Link) {
+	return func(link *Link) {
+		link.lanesChange = lanesChange
+	}
+}
+
+// WithLengthMeters sets underlying geometry [WGS84] length in meters. Warning: it should be called explicitly after setting geometry [WGS84]
+func WithLengthMeters(lengthMeters float64) func(*Link) {
+	return func(link *Link) {
+		link.lengthMeters = lengthMeters
+	}
+}
+
+// WithSourceNodeID sets source node identifier. Should not be used since it necessary part of NewLinkFrom(...).
+func WithSourceNodeID(nodeID gmns.NodeID) func(*Link) {
+	return func(link *Link) {
+		link.sourceNodeID = nodeID
+	}
+}
+
+// WithTargetNodeID sets target node identifier. Should not be used since it necessary part of NewLinkFrom(...).
+func WithTargetNodeID(nodeID gmns.NodeID) func(*Link) {
+	return func(link *Link) {
+		link.targetNodeID = nodeID
+	}
+}
+
+// WithMacroNodeID sets parent macro node identifier
+func WithMacroNodeID(macroNodeID gmns.NodeID) func(*Link) {
+	return func(link *Link) {
+		link.macroNodeID = macroNodeID
+	}
+}
+
+// WithMacroLinkID sets parent macro link identifier
+func WithMacroLinkID(macroLinkID gmns.LinkID) func(*Link) {
+	return func(link *Link) {
+		link.macroLinkID = macroLinkID
+	}
+}
+
+// WithSegmentIdx sets the segment index (inside of parent macroscopic link) for the given link.
+func WithSegmentIdx(segmentIdx int) func(*Link) {
+	return func(link *Link) {
+		link.segmentIdx = segmentIdx
+	}
+}
+
+// WithIsConnection sets whether the given link is a connection between two other mesoscopic links.
+func WithIsConnection(isConnection bool) func(*Link) {
+	return func(link *Link) {
+		link.isConnection = isConnection
+	}
+}
+
+// WithMovementID sets the movement identifier for the link.
+func WithMovementID(movementID gmns.MovementID) func(*Link) {
+	return func(link *Link) {
+		link.movementID = movementID
+	}
+}
+
+// WithMovementCompositeType sets the movement composite type for the link.
+func WithMovementCompositeType(movementCompositeType movement.MovementCompositeType) func(*Link) {
+	return func(link *Link) {
+		link.movementCompositeType = movementCompositeType
+	}
+}
+
+// WithMovementMesoLinkIncome sets the incoming mesoscopic link ID for the link.
+func WithMovementMesoLinkIncome(movementMesoLinkIncome gmns.LinkID) func(*Link) {
+	return func(link *Link) {
+		link.movementMesoLinkIncome = movementMesoLinkIncome
+	}
+}
+
+// WithMovementMesoLinkOutcome sets the outgoing mesoscopic link ID for the link.
+func WithMovementMesoLinkOutcome(movementMesoLinkOutcome gmns.LinkID) func(*Link) {
+	return func(link *Link) {
+		link.movementMesoLinkOutcome = movementMesoLinkOutcome
+	}
+}
+
+// WithMovementIncomeLaneStartSeqID sets the income lane start sequence ID for the link.
+func WithMovementIncomeLaneStartSeqID(movementIncomeLaneStartSeqID int) func(*Link) {
+	return func(link *Link) {
+		link.movementIncomeLaneStartSeqID = movementIncomeLaneStartSeqID
+	}
+}
+
+// WithMovementOutcomeLaneStartSeqID sets the outcome lane start sequence ID for the link.
+func WithMovementOutcomeLaneStartSeqID(movementOutcomeLaneStartSeqID int) func(*Link) {
+	return func(link *Link) {
+		link.movementOutcomeLaneStartSeqID = movementOutcomeLaneStartSeqID
+	}
+}
+
+// WithControlType sets the control type for the link.
+func WithControlType(controlType types.ControlType) func(*Link) {
+	return func(link *Link) {
+		link.controlType = controlType
+	}
+}
+
+// WithLinkType sets the link typk
+func WithLinkType(linkType types.LinkType) func(*Link) {
+	return func(link *Link) {
+		link.linkType = linkType
+	}
+}
+
+// WithFreeSpeed sets free flow speed for the link
+func WithFreeSpeed(freeSpeed float64) func(*Link) {
+	return func(link *Link) {
+		link.freeSpeed = freeSpeed
+	}
+}
+
+// WithCapacity sets max capacity for the link
+func WithCapacity(capacity int) func(*Link) {
+	return func(link *Link) {
+		link.capacity = capacity
+	}
+}
+
+// WithAllowedAgentTypes sets allowed agent types for the link. Warning: it does copy argument
+func WithAllowedAgentTypes(allowedAgentTypes []types.AgentType) func(*Link) {
+	return func(link *Link) {
+		link.allowedAgentTypes = make([]types.AgentType, len(allowedAgentTypes))
+		copy(link.allowedAgentTypes, allowedAgentTypes)
+	}
 }
